7816c4673f22e8d17b3a172b02f10fe13bd6b343
Philani Manatha <philanimanatha@outlook.com>
2023-12-14 18:58:48 +0000

    adding new repository with basic shell

---
+++ ex/ex1/README.md
@@ -0,0 +1,3 @@
+# Exercise: fork, wait and execve 
+create a program that creates five different child processed, but it child process must have the same father, each father cannot be started without the other child process being terminated
+
---
+++ ex/ex1/fivec.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - main function
+ * Returns: always 0
+ */
+int main(void)
+{
+	pid_t fpid, cpid;
+	pid_t process;
+	int status;
+
+	fpid = getpid();
+	cpid = getpid();
+
+	while (cpid <= fpid + 5)
+	{
+		process = fork();
+		cpid = getpid();
+		if ( cpid > fpid + 5)
+		{
+			wait(&status);
+		}
+		
+	}
+
+	if (process == -1)
+	{
+		printf(" Fork process failed");
+	}
+	
+	if (process == 0)
+	{
+		cpid = getpid();
+		fpid = getppid();
+		printf("In child pid: %u and parent pid %u\n", cpid, fpid);
+
+	}
+	else
+	{
+		wait(&status);
+		printf("father process %u\n", fpid);
+	}
+	printf("ending process: %u\n",cpid);
+
+	return (0);
+}
